ebab340595e4423fb7f9b5812a27871e99ced6ff
jjmjyrihs <1104137128@gm.kuas.edu.tw>
2017-04-28 15:25:31 +0800

    3.6看完

---
+++ eSaleDao/OrderDao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSaleDao
+{
+    public class OrderDao
+    {
+        /// <summary>
+        /// 依訂單編號取得訂單資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public eSaleModel.Order GetOrderById(string id)
+        {
+            return new eSaleModel.Order()
+            {
+                CustId = "GOGOGO",
+                CustName = "IIII",
+                OrderId = "1"
+            };
+        }
+    }
+}
